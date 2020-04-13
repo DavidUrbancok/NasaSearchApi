3c6c00b7a286f39fc9af549e49acf7b6bd9ff6d1
David Urbancok <urbancok.david@outlook.com>
2020-04-13 19:16:45 +0200

    Add project
    Signed-off-by: David Urbancok <urbancok.david@outlook.com>

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace NASAImageGalleryREST
+{
+    public class Program
+    {
+        private static readonly HttpClient Client = new HttpClient();
+
+        public static async Task Main()
+        {
+            await GetMarsImages();
+
+            Console.WriteLine(Environment.NewLine);
+
+            await GetLinksToVideos();
+        }
+
+        /// <summary>
+        /// Reads the response from <paramref name="url"/> and deserializes it into an object of <typeparamref name="T"/> type.
+        /// </summary>
+        /// <param name="url">URL to send the request to.</param>
+        /// <typeparam name="T">Response object type.</typeparam>
+        /// <returns>Returns a deserialized object of the HTTPS response.</returns>
+        private static async Task<T> ReadContent<T>(string url)
+        {
+            string content = null;
+            var response = await Client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                content = await response.Content.ReadAsStringAsync();
+            }
+
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        /// <summary>
+        /// Gets five images from 2018 where either Mars' surface is visible or the picture was taken on Mars' surface.
+        /// </summary>
+        private static async Task GetMarsImages()
+        {
+            const string url = "https://images-api.nasa.gov/search?q=Mars&description=Mars%20surface&year_start=2018&year_end=2018";
+
+            var repository = await ReadContent<NasaRepository>(url);
+            var itemLinks = repository.collection.items.Take(5);
+
+            foreach (var itemLink in itemLinks)
+            {
+                var link = itemLink.links
+                                   .Where(link => link.render.Equals("image", StringComparison.OrdinalIgnoreCase))
+                                   .FirstOrDefault();
+
+                Console.WriteLine(link.href);
+            }
+        }
+
+        /// <summary>
+        /// Checks if 'video' media types contain a link to a video.
+        /// </summary>
+        private static async Task GetLinksToVideos()
+        {
+            const string url = "https://images-api.nasa.gov/search?q=Mars&year_start=2018&year_end=2018&media_type=video";
+
+            var repository = await ReadContent<NasaRepository>(url);
+            var videoCollectionUrls = repository.collection.items.Select(item => item.href).Take(5); // Limit the number of requests
+
+            foreach (var videoUrl in videoCollectionUrls)
+            {
+                var links = await ReadContent<List<string>>(videoUrl);
+
+                if (!links.Any(link => link.EndsWith(".mp4", StringComparison.OrdinalIgnoreCase)))
+                {
+                    Console.WriteLine($"{videoUrl} does not contain a link to a video.");
+
+                    return;
+                }
+            }
+
+            Console.WriteLine("All links seem fine.");
+        }
+    }
+}
---
+++ Repositories/NasaRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NASAImageGalleryREST
+{
+    /// <summary>
+    /// Represents the structure of a JSON response from the NASA Gallery API.
+    /// </summary>
+    public class NasaRepository
+    {
+        public class Link
+        {
+            public string rel { get; set; }
+            public string href { get; set; }
+            public string render { get; set; }
+        }
+
+        public class Datum
+        {
+            public string title { get; set; }
+            public string center { get; set; }
+            public string nasa_id { get; set; }
+            public DateTime date_created { get; set; }
+            public List<string> keywords { get; set; }
+            public string description { get; set; }
+            public string media_type { get; set; }
+            public string description_508 { get; set; }
+            public string secondary_creator { get; set; }
+            public string location { get; set; }
+            public string photographer { get; set; }
+            public List<string> album { get; set; }
+        }
+
+        public class Item
+        {
+            public List<Link> links { get; set; }
+            public string href { get; set; }
+            public List<Datum> data { get; set; }
+        }
+
+        public class Link2
+        {
+            public string rel { get; set; }
+            public string href { get; set; }
+            public string prompt { get; set; }
+        }
+
+        public class Metadata
+        {
+            public int total_hits { get; set; }
+        }
+
+        public class Collection
+        {
+            public List<Item> items { get; set; }
+            public List<Link2> links { get; set; }
+            public string version { get; set; }
+            public string href { get; set; }
+            public Metadata metadata { get; set; }
+        }
+
+        public Collection collection { get; set; }
+    }
+}
